edba1b8baa80b97bac58749010975f94c1a83bee
Tommaso Allegretti <allegretti.tommaso@gmail.com>
2025-04-03 18:41:11 +0200

    edit construction message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
                     <div class="grid grid-cols-1 gap-6 lg:gap-8">
                         <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
-                                <h2 class="mt-2 text-xl font-semibold text-gray-900 dark:text-white">Sito in costruzione</h2>
+                                <h2 class="mt-2 text-xl font-semibold text-gray-900 dark:text-white">Sito in costruzione.</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                     Stiamo lavorando per voi!
