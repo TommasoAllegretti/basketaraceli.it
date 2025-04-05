5d2b8cde32956e193491bed604da33f2f973c6db
Tommaso Allegretti <allegretti.tommaso@gmail.com>
2025-04-05 21:18:29 +0200

    added Breeze auth system

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,9 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.front')
 
 @section('content')
 
-<div class="flex justify-center h-[calc(100vh-72px)] lg:h-[calc(100vh-112px)] bg-center bg-no-repeat bg-cover" style="background-image: url({{asset('assets/hero-bg.jpeg')}})">
-    <img src="{{asset('assets/logo_araceli.png')}}" class="h-48 lg:h-96 my-auto" title="Logo Araceli" alt="Logo Araceli" />
-</div>
+    <div class="flex justify-center h-[calc(100vh-72px)] lg:h-[calc(100vh-112px)] bg-center bg-no-repeat bg-cover"
+        style="background-image: url({{asset('assets/hero-bg.jpeg')}})">
+        <img src="{{asset('assets/logo_araceli.png')}}" class="h-48 lg:h-96 my-auto" title="Logo Araceli"
+            alt="Logo Araceli" />
+    </div>
 
 @endsection
