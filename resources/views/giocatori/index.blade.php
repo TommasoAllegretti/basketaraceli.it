73000c68f867060431c53ac738e95687c0807080
Tommaso Allegretti <allegretti.tommaso@gmail.com>
2025-04-06 22:33:29 +0200

    added players page mobile link

--- resources/views/giocatori/index.blade.php
+++ resources/views/giocatori/index.blade.php
@@ -15,95 +15,104 @@
                 </a>
 
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3">
-                                    #
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Nome
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Posizione
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Altezza
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    PPG
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    RPG
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    APG
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-right">
-                                    Azioni
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
 
-                            @foreach ($players as $player)
+                    @if ($players->isEmpty())
+                        <p class="text-center text-gray-500 dark:text-gray-400">
+                            {{ __('Nessun giocatore trovato.') }}
+                        </p>
+                    @endif
 
-                                <tr
-                                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                    <th scope="row"
-                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        {{ $player->jersey_number }}
+                    @if (!$players->isEmpty())
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        #
                                     </th>
-                                    <td class="px-6 py-4">
-                                        {{ $player->name }}
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        {{ $player->position }}
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        {{ $player->height_cm }}
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        {{ $player->points_per_game }}
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        {{ $player->rebounds_per_game }}
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        {{ $player->assists_per_game }}
-                                    </td>
-                                    <td class="px-6 py-4 text-right">
-                                        <form action="{{ route('giocatori.destroy', $player->id) }}" method="POST"
-                                            class="space-x-2">
-
-
-
-                                            <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                                                href="{{ route('giocatori.show', $player->id) }}">Show</a>
-
-
-
-                                            <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                                                href="{{ route('giocatori.edit', $player->id) }}">Edit</a>
-
-
-
-                                            @csrf
-
-                                            @method('DELETE')
-
-
-
-                                            <button type="submit"
-                                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</button>
-
-                                        </form>
-                                    </td>
+                                    <th scope="col" class="px-6 py-3">
+                                        Nome
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Posizione
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Altezza
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        PPG
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        RPG
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        APG
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-right">
+                                        Azioni
+                                    </th>
                                 </tr>
+                            </thead>
+                            <tbody>
 
-                            @endforeach
-                        </tbody>
-                    </table>
+                                @foreach ($players as $player)
+
+                                    <tr
+                                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ $player->jersey_number }}
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            {{ $player->name }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $player->position }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $player->height_cm }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $player->points_per_game }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $player->rebounds_per_game }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $player->assists_per_game }}
+                                        </td>
+                                        <td class="px-6 py-4 text-right">
+                                            <form action="{{ route('giocatori.destroy', $player->id) }}" method="POST"
+                                                class="space-x-2">
+
+
+
+                                                <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                                                    href="{{ route('giocatori.show', $player->id) }}">Show</a>
+
+
+
+                                                <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                                                    href="{{ route('giocatori.edit', $player->id) }}">Edit</a>
+
+
+
+                                                @csrf
+
+                                                @method('DELETE')
+
+
+
+                                                <button type="submit"
+                                                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</button>
+
+                                            </form>
+                                        </td>
+                                    </tr>
+
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endif
                 </div>
 
             </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -75,6 +75,9 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('giocatori.index')" :active="request()->routeIs('giocatori.index')">
+                {{ __('Giocatori') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
