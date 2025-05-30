8dfec3d045c6b71ff4798523691b7a885eea046f
Tommaso Allegretti <allegretti.tommaso@gmail.com>
2025-05-30 10:34:58 +0200

    removed CRM blade pages

--- routes/web.php
+++ routes/web.php
@@ -42,22 +42,4 @@ Route::get('/contatti', function () {
     return view('pages/contatti');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::resource('leagues', LeagueController::class)->middleware(['auth', 'verified', 'admin']);
-Route::resource('teams', TeamController::class)->middleware(['auth', 'verified', 'admin']);
-Route::get('/teams/{team}/players', [TeamController::class, 'getPlayers'])->name('teams.players');
-Route::resource('players', PlayerController::class)->middleware(['auth', 'verified', 'admin']);
-Route::resource('games', GameController::class)->middleware(['auth', 'verified', 'admin']);
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/my-player', [PlayerController::class, 'show'])->name('player.show');
-});
-
 require __DIR__ . '/auth.php';
