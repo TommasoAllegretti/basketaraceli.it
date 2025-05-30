df9cbe438442c5fa8805b116da1d19ead3c59875
Tommaso Allegretti <allegretti.tommaso@gmail.com>
2025-05-30 17:21:02 +0200

    restored public area routes and style

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return view('pages/welcome');
+});
+
+Route::get('/home', function () {
+    return view('pages/home');
+});
+
+Route::get('/eventi', function () {
+    return view('pages/eventi');
+});
+
+Route::get('/squadre', function () {
+    return view('pages/squadre');
+});
+
+Route::get('/organizzazione', function () {
+    return view('pages/organizzazione');
+});
+
+Route::get('/contatti', function () {
+    return view('pages/contatti');
 });
 
 require __DIR__.'/auth.php';
