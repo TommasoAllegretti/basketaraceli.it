1497fb36c01d5cc75ca2b31da4731d6d6efbabd2
Tommaso Allegretti <allegretti.tommaso@gmail.com>
2025-06-10 21:30:54 +0200

    fixed css build + tailwind dark mode

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -11,7 +11,7 @@
     <link rel="preconnect" href="https://rsms.me/">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
-    @vite('resources/css/app.css')
+    @vite('resources/sass/app.scss')
 </head>
 
 <body>
